ed79087fd5a991b3acd1e37cad8b99ae706ec802
Kevin Kelly <kvinklly@gmail.com>
2018-07-05 11:42:48 -0500

    move action working

--- game/index.js
+++ game/index.js
@@ -1,6 +1,30 @@
 import {Map} from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+const MOVE = 'MOVE'
+
+const initialState = {
+  turn: 'X',
+  board: Map()
+}
+
+/* action creator */
+
+export const move = (player, position) => {
+  return {
+    type: MOVE,
+    player,
+    position
+  }
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case MOVE:
+      return {
+        turn: state.turn === 'X' ? 'O' : 'X',
+        board: state.board.setIn(action.position, action.player)
+      }
+    default:
+      return state
+  }
 }
